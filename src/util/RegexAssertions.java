d0de539be1898d5eb063ea567e0bfeeaad7e99a2
Nate Riffe <inkblot@movealong.org>
2011-10-23 23:12:13 -0500

    Create a project for shared test suite code... which is just RegexAssertions for now

---
+++ src/util/RegexAssertions.java
@@ -0,0 +1,47 @@
+// Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
+// Released under the terms of the CPL Common Public License version 1.0.
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.fail;
+
+public final class RegexAssertions {
+
+    public static void assertMatches(String regexp, String string) {
+        assertHasRegexp(regexp, string);
+    }
+
+    public static void assertNotMatches(String regexp, String string) {
+        assertDoesNotHaveRegexp(regexp, string);
+    }
+
+    public static void assertHasRegexp(String regexp, String output) {
+        Matcher match = Pattern.compile(regexp, Pattern.MULTILINE | Pattern.DOTALL).matcher(output);
+        boolean found = match.find();
+        if (!found)
+            fail("The regexp <" + regexp + "> was not found in: " + output + ".");
+    }
+
+    public static void assertDoesNotHaveRegexp(String regexp, String output) {
+        Matcher match = Pattern.compile(regexp, Pattern.MULTILINE).matcher(output);
+        boolean found = match.find();
+        if (found)
+            fail("The regexp <" + regexp + "> was found.");
+    }
+
+    public static void assertSubString(String substring, String string) {
+        if (!string.contains(substring))
+            fail("substring '" + substring + "' not found in string '" + string + "'.");
+    }
+
+    public static void assertNotSubString(String subString, String string) {
+        if (string.contains(subString))
+            fail("expecting substring:'" + subString + "' in string:'" + string + "'.");
+    }
+
+    public static String divWithIdAndContent(String id, String expectedDivContent) {
+        return "<div.*?id=\"" + id + "\".*?>" + expectedDivContent + "</div>";
+    }
+}
